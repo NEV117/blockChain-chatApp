55517cd531fa386644e9c0906de89e18b2f9a40a
NEV117 <116839735+NEV117@users.noreply.github.com>
2023-11-06 10:50:59 -0500

    login screen

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,8 +2,24 @@ import styles from '../styles/Home.module.css'
 import SideBar from '../components/Sidebar'
 import ConversationList from '../components/ConversationList'
 import ChatView from '../components/ChatView'
+import { DiscordContext } from "../context/context";
+import { useContext, useEffect  } from "react";
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  
+  const { roomName, currentAccount, connectWallet } =
+    useContext(DiscordContext);
+    const router = useRouter();
+
+  console.log(currentAccount)
+  
+  useEffect(() => {
+    if (!currentAccount) {
+      router.push('/auth');
+    }
+  }, [currentAccount, router]);
+
   return (
     <div className={styles.wrapper}>
       <SideBar />
